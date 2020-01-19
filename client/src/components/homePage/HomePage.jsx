054a8f04b3b77b355af63f089cc890a72628bfd6
Kristyna Svecova <kristyna@rewardops.com>
2020-01-19 15:25:29 -0500

    Remove auth buttons

--- client/src/components/homePage/HomePage.jsx
+++ client/src/components/homePage/HomePage.jsx
@@ -8,19 +8,6 @@ const Header = () => {
         <p className="lead">
           This is a site where I can easily navigate through my personal collection of recipes.
         </p>
-        <hr className="my-3" />
-        <p>Sign in or register to add new recipes</p>
-        <div className="d-flex">
-          <a className="btn btn-regular btn-md" href="/recipes/new" role="button">
-            Add New Recipe
-          </a>
-          <a className="btn btn-regular btn-md mr-2 ml-auto" href="/login" role="button">
-            Sign In
-          </a>
-          <a className="btn btn-regular btn-md" href="/register" role="button">
-            Register
-          </a>
-        </div>
       </header>
     </div>
   );
