434c2778cb2b815e25c8c33bfb6ea0dad3569b81
Kristyna Svecova <kristyna@rewardops.com>
2020-01-21 14:35:15 -0500

    Fetch recipes from homepage

--- client/src/components/homePage/HomePage.jsx
+++ client/src/components/homePage/HomePage.jsx
@@ -1,6 +1,16 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Header = () => {
+  const [recipes, setRecipes] = useState([]);
+  const [error, setError] = useState({});
+
+  useEffect(() => {
+    fetch('http://localhost:8080/recipes')
+      .then(response => response.json())
+      .then(res => setRecipes(res))
+      .catch(err => setError(err));
+  }, []);
+
   return (
     <div className="jumbotron jumbotron-fluid py-4">
       <header className="container">
--- server/index.js
+++ server/index.js
@@ -1,9 +1,12 @@
-const express = require ('express');
+const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-mongoose.connect('mongodb://localhost/recipes', { useUnifiedTopology: true, useNewUrlParser: true });
+mongoose.connect('mongodb://localhost/recipes', {
+  useUnifiedTopology: true,
+  useNewUrlParser: true
+});
 
 let RecipeSchema = new Schema({
   title: {
@@ -12,17 +15,17 @@ let RecipeSchema = new Schema({
   },
   image: String,
   ingredients: [
-      {
-          name:  {
-            en: String,
-            cs: String
-          },
-          amount: Number,
-          unit: {
-            en: String,
-            cs: String
-          }
+    {
+      name: {
+        en: String,
+        cs: String
+      },
+      amount: Number,
+      unit: {
+        en: String,
+        cs: String
       }
+    }
   ],
   directions: {
     en: [String],
@@ -32,27 +35,31 @@ let RecipeSchema = new Schema({
   prepTime: Number,
   cookingTime: Number,
   author: {
-      id: {
-          type: mongoose.Schema.Types.ObjectId,
-          rel: 'User'
-      },
-      username: String
+    id: {
+      type: mongoose.Schema.Types.ObjectId,
+      rel: 'User'
+    },
+    username: String
   }
 });
 
 let Recipe = mongoose.model('Recipe', RecipeSchema);
 
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 app.get('/', (req, res) => {
   res.redirect('/recipes');
 });
 
-app.get('/recipes', function (req, res) {
-  Recipe.find({}, function (err, recipes) {
+app.get('/recipes', function(req, res) {
+  Recipe.find({}, function(err, recipes) {
     err ? console.log(err) : res.json(recipes);
-  })
+  });
 });
 
 // start the DB locally: mongod
-app.listen(8080, () =>
-  console.log('Example app listening on port 8080!'),
-);
+app.listen(8080, () => console.log('Example app listening on port 8080!'));
