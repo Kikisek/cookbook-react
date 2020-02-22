c8f36b849b7dfd13ac5e737ef458fbe2350a7748
Kristyna Svecova <kristyna@rewardops.com>
2020-02-21 19:06:27 -0500

    Add router

--- client/src/components/app/App.jsx
+++ client/src/components/app/App.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import './App.css';
 import Homepage from '../homepage/Homepage';
 
 function App() {
   return (
-    <div>
-      <Homepage />
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" render={() => <Redirect to="/recipes" />} />
+        <Route exact path="/recipes" component={Homepage} />
+        <Route path="/recipes/:id">Recipe</Route>
+      </Switch>
+    </Router>
   );
 }
 
